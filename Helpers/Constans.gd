54959137bcfc0c70f331b470d87317154b18a857
Christopher Francisco <christopher.f.almanza@gmail.com>
2017-12-16 11:59:38 -0500

    Improve scrap constant namespace
    Changed to `SCRAP_TYPE_<type>` for readability.

--- Helpers/Constans.gd
+++ Helpers/Constans.gd
@@ -5,10 +5,10 @@ const BULLET_SPEED = 800
 const BULLET_FIRE_RATE = 0.25
 const BULLET_FIRE_RATE_VARIANCE = 0.10
 
-const SCRAP_MISSILE = 0
-const SCRAP_LASER = 1
-const SCRAP_SHIELD = 2
-const SCRAP_WING = 3
+const SCRAP_TYPE_MISSILE = 0
+const SCRAP_TYPE_LASER = 1
+const SCRAP_TYPE_SHIELD = 2
+const SCRAP_TYPE_WING = 3
 
 func _ready():
 	pass
