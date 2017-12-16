048d626bd79f0639888c9f1ab271f24a5f027824
Christopher Francisco <christopher.f.almanza@gmail.com>
2017-12-16 11:56:09 -0500

    Add scrap type constants

--- Helpers/Constans.gd
+++ Helpers/Constans.gd
@@ -5,5 +5,10 @@ const BULLET_SPEED = 800
 const BULLET_FIRE_RATE = 0.25
 const BULLET_FIRE_RATE_VARIANCE = 0.10
 
+const SCRAP_MISSILE = 0
+const SCRAP_LASER = 1
+const SCRAP_SHIELD = 2
+const SCRAP_WING = 3
+
 func _ready():
 	pass
