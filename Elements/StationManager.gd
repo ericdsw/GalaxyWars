8a6faba9d67955dfb15b7fca1260fd11686f535e
Christopher Francisco <christopher.f.almanza@gmail.com>
2017-12-16 12:57:22 -0500

    Initialize station manager inventory

--- Elements/StationManager.gd
+++ Elements/StationManager.gd
@@ -1,5 +1,6 @@
 extends Node
 
+export var instance_name = "StationManager"
 onready var battleship_scene = load("res://Elements/Battleship.tscn")
 
 var economy;
@@ -8,17 +9,10 @@ var scrap_inventory;
 func _ready():
     _init_economy()
     _init_scrap_inventory()
+    _print_debug_info()
 
-func _init_economy()
-    economy = ECONOMY_INITIAL_AMOUNT
-
-func _init_inventory()
-    scrap_inventory = {
-	"missile": SCRAP_TYPE_MISSILE_INITIAL_AMOUNT,
-	"laser": SCRAP_TYPE_LASER_INITIAL_AMOUNT,
-	"shield": SCRAP_TYPE_SHIELD_INITIAL_AMOUNT,
-	"wings": SCRAP_TYPE_WINGS_INITIAL_AMOUNT
-    }
+func _on_ship_destroyed():
+	TimerGenerator.create_timer(3, "spawn_battleship", self).start()
 
 func spawn_battleship():
 	var battleship_instance = battleship_scene.instance()
@@ -33,5 +27,22 @@ func increment_economy(scrap_amount):
 func increment_power_up(scrap_amount, scrap_type):
     scrap_inventory[scrap_type] = scrap_inventory[scrap_type] - 1
 
-func _on_ship_destroyed():
-	TimerGenerator.create_timer(3, "spawn_battleship", self).start()
+
+func _init_economy():
+    economy = Constants.ECONOMY_INITIAL_AMOUNT
+
+func _init_scrap_inventory():
+    scrap_inventory = {
+	"missile": Constants.SCRAP_TYPE_MISSILE_INITIAL_AMOUNT,
+	"laser": Constants.SCRAP_TYPE_LASER_INITIAL_AMOUNT,
+	"shield": Constants.SCRAP_TYPE_SHIELD_INITIAL_AMOUNT,
+	"wings": Constants.SCRAP_TYPE_WINGS_INITIAL_AMOUNT
+    }
+
+func _print_debug_info():
+    print(instance_name + " properties:")
+    print("missile: " + str(scrap_inventory["missile"]))
+    print("laser: " + str(scrap_inventory["laser"]))
+    print("shield: " + str(scrap_inventory["shield"]))
+    print("wings: " + str(scrap_inventory["wings"]))
+    print("------------------------------------")
