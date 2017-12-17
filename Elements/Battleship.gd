e2e22ed5a1c7e93f13469e4a12be2bf866bd86f0
Christopher Francisco <christopher.f.almanza@gmail.com>
2017-12-17 12:11:08 -0500

    Improve code on Battleship

--- Elements/Battleship.gd
+++ Elements/Battleship.gd
@@ -4,22 +4,18 @@ const scrap_types = ["MissileScrap.tscn", "LaserScrap.tscn", "ShieldScrap.tscn",
 
 onready var bullet_scene = load("res://Elements/Bullet.tscn")
 onready var sprite = get_node("Sprite").get_texture()
-
-# onready var missile_launcher_pos = get_node("MissileLauncherPos").get_pos()
-# onready var laser_beam_pos = get_node("LaserBearmPos").get_pos()
-# onready var wings_pos = get_node("WingsPos").get_pos()
-
+onready var stat_provider = get_node("StatProvider")
+	
+# Fixed positions for power ups
 var missile_launcher_pos = Vector2(63, 61)
 var laser_beam_pos = Vector2(-30, -63)
 var wings_pos = Vector2(-26, 21)
 
-var bullet_starting_pos
 var bullet_fire_timer
 
 signal destroyed()
 
-# TODO: fix this
-var current_level = 3
+var current_level = BATTLESHIP_INITIAL_LEVEL
 var max_hp
 var hp
 var accuracy
@@ -31,22 +27,11 @@ var team_group_name
 var wings_blessing_enabled = false
 
 func _ready():
-	set_stats()
+	_init_stats()
 
 	TimerGenerator.create_timer(0.2, "receive_damage", self, false, [50]).start()
 	bullet_fire_timer = TimerGenerator.create_timer(Constants.BULLET_FIRE_RATE, "spawn_bullet", self, false)
 	bullet_fire_timer.start()
-
-func set_stats():
-	var stat_provider = get_node("StatProvider")
-	
-	max_hp = stat_provider.get_max_hp_for_level(current_level)
-	hp = max_hp
-	accuracy = stat_provider.get_accuracy_for_level(current_level)
-	evasion = stat_provider.get_evasion_for_level(current_level)
-
-	if (wings_blessing_enabled):
-		evasion += stat_provider.get_wing_evasion_for_level(current_level)
 
 func set_team_group_name(team_group_name):
 	self.team_group_name = team_group_name
@@ -116,3 +101,16 @@ func _add_scrap_to_world(scrap):
 	scrap.set_pos(get_pos())
 	get_node("/root/Game").add_child(scrap)
 	scrap.apply_impulse(Vector2(15, 15), _generate_random_force())
+
+func _init_stats():
+	max_hp = stat_provider.get_max_hp_for_level(current_level)
+	accuracy = stat_provider.get_accuracy_for_level(current_level)
+	evasion = stat_provider.get_evasion_for_level(current_level)
+
+	_fill_hp()
+
+	if (wings_blessing_enabled):
+		evasion += stat_provider.get_wing_evasion_for_level(current_level)
+
+func _fill_hp():
+	hp = max_hp
