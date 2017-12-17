2a18631087d09e8d0363849653ac37cea4666d51
Christopher Francisco <christopher.f.almanza@gmail.com>
2017-12-16 19:39:22 -0500

    Add skeleton to drop scraps on station

--- Elements/Player.gd
+++ Elements/Player.gd
@@ -5,22 +5,34 @@ export (String) var team_group_name
 
 var station_manager
 var scrap_inventory
-var station_active_flags
+var active_station
 
 onready var area = get_node("Area2D")
 
 func _ready():
+	set_process_input(true)
 	set_fixed_process(true)
 	_init_scrap_inventory()
 	_init_station_active_flags()
 	_connect_to_signals()
 	_print_debug_info()
 	
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		if active_station == "power_up_station":
+			_drop_scraps_to_power_up_station()
+
+		elif active_station == "economy_station":
+			_drop_scraps_to_economy_station()
+
+		# TODO: else attack
+	
 func _fixed_process(delta):
 	_handle_movement(delta)
 
 func _handle_movement(delta):
 	var direction = Vector2(0,0)
+
 	
 	if Input.is_action_pressed("ui_left"):
 		direction = Vector2(-1, 0)
@@ -39,10 +51,7 @@ func _init_scrap_inventory():
 	}
 
 func _init_station_active_flags():
-	station_active_flags = {
-		"economy_station": false,
-		"power_up_station": false,
-	}
+	active_station = null
 
 func _connect_to_signals():
 	area.connect("area_enter", self, "_on_area_enter")
@@ -50,31 +59,29 @@ func _connect_to_signals():
 
 func _on_area_enter(area):
 	if area.is_in_group(team_group_name) && area.is_in_group("power_up_station"):
-		_on_power_up_station_enter()
+		_on_station_enter("power_up_station")
 
 	if area.is_in_group(team_group_name) && area.is_in_group("economy_station"):
-		_on_economy_station_enter()
+		_on_station_enter("economy_station")
 
 func _on_area_exit(area):
 	if area.is_in_group(team_group_name) && area.is_in_group("power_up_station"):
-		_on_power_up_station_exit()
+		_on_station_exit()
 
 	if area.is_in_group(team_group_name) && area.is_in_group("economy_station"):
-		_on_economy_station_exit()
+		_on_station_exit()
 
-func _on_power_up_station_enter():
-	print("power up enter")
-	station_active_flags["power_up_station"] = true
+func _on_station_enter(station):
+	active_station = station
 
-func _on_economy_station_enter():
-	station_active_flags["economy_station"] = true
+func _on_station_exit():
+	active_station = null
 
-func _on_power_up_station_exit():
-	print("power up exit")
-	station_active_flags["power_up_station"] = false
+func _drop_scraps_to_power_up_station():
+	print("Scraps dropped to power up station")
 
-func _on_economy_station_exit():
-	station_active_flags["economy_station"] = false
+func _drop_scraps_to_economy_station():
+	print("Scraps dropped to economy station")
 
 func _print_debug_info():
 	print(instance_name + " properties:")
--- Elements/Station/Station.gd
+++ Elements/Station/Station.gd
@@ -6,7 +6,28 @@ signal player_entered()
 signal player_exited()
 
 func _ready():
+	_connect_to_signals()
 	_print_debug_info()
+
+func _connect_to_signals():
+	connect("area_enter", self, "_on_area_enter")
+	connect("area_exit", self, "_on_area_exit")
+
+func _on_area_enter(area):
+	if area.is_in_group(team_group_name) && area.is_in_group("player"):
+		_on_player_enter()
+
+func _on_area_exit(area):
+	if area.is_in_group(team_group_name) && area.is_in_group("player"):
+		_on_player_exit()
+
+func _on_player_enter():
+	# TODO: light animation
+	pass
+
+func _on_player_exit():
+	# TODO: light animation
+	pass
 	
 func _print_debug_info():
 	print(get_name())
